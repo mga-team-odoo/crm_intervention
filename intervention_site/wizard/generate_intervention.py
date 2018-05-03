5f3948083ebe3c41e65612f004e700db9413077c
Christophe CHAUVET <christophe.chauvet@gmail.com>
2018-05-03 22:55:27 +0200

    Add same improvement like site

--- intervention_site/wizard/generate_intervention.py
+++ intervention_site/wizard/generate_intervention.py
@@ -36,7 +36,8 @@ class GenerateIntervention(orm.TransientModel):
                 part_id = eq.site_id.customer_id.id
 
             part_vals = inter_obj.onchange_partner_intervention_id(cr, uid, [], part_id)
-            int_args = {
+            int_args = part_vals['value']
+            int_args.update({
                 'name': this.name + ' ' + eq.name,
                 'section_id': this.section_id and this.section_id.id or False,
                 'user_id': this.user_id and this.user_id.id or False,
@@ -45,17 +46,29 @@ class GenerateIntervention(orm.TransientModel):
                 'partner_id': part_id,
                 'equipment_id': eq.id,
                 'type_id': this.type_id and this.type_id.id or False,
-            }
+            })
             if this.begin_date:
                 int_args['date_planned_end'] = inter_obj.onchange_planned_duration(
                     cr, uid, [], 1.0, this.begin_date)['value']['date_planned_end']
-            int_args.update(part_vals['value'])
-            if eq.site_id:
-                int_args['site_id'] = eq.site_id.id
-                if not this.section_id and eq.site_id.section_id:
-                    int_args['section_id'] = eq.site_id.section_id.id
-                if eq.site_id.partner_id:
-                    int_args['partner_shipping_id'] = eq.site_id.partner_id.id
+            site = eq.site_id
+            if site:
+                int_args['site_id'] = site.id
+                if not this.section_id and site.section_id:
+                    int_args['section_id'] = site.section_id.id
+                if site.partner_id:
+                    int_args['partner_shipping_id'] = site.partner_id.id
+
+                if site.contract_id:
+                    int_args.update({
+                        'contract_id': site.contract_id.id,
+                        'partner_invoice_id': site.partner_id and site.partner_id.id or part_id,
+                    })
+                else:
+                    int_args.update({
+                        'contract_id': False,
+                        'partner_invoice_id': part_id,
+                    })
+
             int_ids.append(inter_obj.create(cr, uid, int_args, context=context))
 
         return inter_obj.open_intervention(cr, uid, int_ids, context=context)
